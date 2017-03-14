5aeb0b6a256f608b3d21e0c48dc17e4c29206e03
niuguanx <guangyuanx.niu@intel.com>
2017-03-14 13:15:59 +0800

    V2EX: base ui
    Signed-off-by: niuguanx <guangyuanx.niu@intel.com>

---
+++ app/src/main/java/test/demo/gyniu/v2ex/BottomTabBehavior.java
@@ -0,0 +1,27 @@
+package test.demo.gyniu.v2ex;
+
+import android.content.Context;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by uiprj on 17-3-14.
+ */
+public class BottomTabBehavior extends CoordinatorLayout.Behavior<View> {
+    public BottomTabBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+        return dependency instanceof AppBarLayout;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
+        float translationY = Math.abs(dependency.getTop());
+        child.setTranslationY(translationY);
+        return true;
+    }
+}
--- app/src/main/java/test/demo/gyniu/v2ex/MainActivity.java
+++ app/src/main/java/test/demo/gyniu/v2ex/MainActivity.java
@@ -1,13 +1,29 @@
 package test.demo.gyniu.v2ex;
 
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by uiprj on 17-3-13.
+ */
+public class MainActivity extends AppCompatActivity{
+    private static final String TAG = "MainActivity";
+
+    private AppBarLayout mAppBarLayout;
+    private Toolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAppBarLayout = (AppBarLayout) findViewById(R.id.appBar);
+        mToolBar = (Toolbar) mAppBarLayout.findViewById(R.id.toolbar);
+        mToolBar.setTitle(R.string.app_name);
+        setSupportActionBar(mToolBar);
     }
+
 }
